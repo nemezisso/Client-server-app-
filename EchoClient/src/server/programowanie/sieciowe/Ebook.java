9fbf047f2188f5d39b3e064976e466aae570e454
nemezisso <b.kochanowski11@gmail.com>
2017-04-24 09:08:37 +0200

    Update Ebook.java

--- EchoClient/src/server/programowanie/sieciowe/Ebook.java
+++ EchoClient/src/server/programowanie/sieciowe/Ebook.java
@@ -1,8 +1,5 @@
 package server.programowanie.sieciowe;
-/** Klasa Ebook dziedziczy po klasie DigitalBook
- * 
- * @author Bartek
- */
+
 public class Ebook extends DigitalBook {
     private String format;
     
@@ -12,18 +9,7 @@ public class Ebook extends DigitalBook {
         super(ISBNnumber,title,author,publisher,value,size,DRM,quality);
         this.format="PDF";
     }
-     /**
-     * Konstruktor klasy Ebook
-     * @param ISBNnumber numer ISBN książki
-     * @param title tytuł książki
-     * @param author autor książki
-     * @param publisher wydawca książki
-     * @param value cena książki
-     * @param size rozmiar w kb książki
-     * @param DRM zabezpieczenie w postaci DRM książki (true/false)
-     * @param quality jakość książki
-     * @param format format książki (mobi, pdf itd.)
-     */
+
     public Ebook(int ISBNnumber, String title, String author,
                 String publisher, double value,double size,boolean DRM,
                 String quality,String format){
