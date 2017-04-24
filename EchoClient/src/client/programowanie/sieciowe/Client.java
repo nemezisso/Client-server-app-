a1d4e84f97eebddbd7c51aeba93b1c66fe7e165d
nemezisso <b.kochanowski11@gmail.com>
2017-04-24 09:01:01 +0200

    Update Client.java

--- EchoClient/src/client/programowanie/sieciowe/Client.java
+++ EchoClient/src/client/programowanie/sieciowe/Client.java
@@ -5,11 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-/**
- * Klasa klienta tekstowego
- * 
- * @author Bartek
- */
+
 public class Client {
 
     public static void main(String[] args) {
